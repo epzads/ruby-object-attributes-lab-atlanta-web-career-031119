58415b4b47e49f643209f1d62af3844933d57d28
favorable-vector-1743 <epzads@gmail.com>
2019-03-13 17:18:42 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,33 @@
+class Dog 
+
+def intialize(name = "dog")
+  
+@name = name   
+  
+  
+end  
+
+def name 
+  
+  
+  @name 
+end 
+
+def name=(new_name)
+
+@name = new_name 
+end 
+
+def breed=(breed_name)
+
+@breed = breed_name 
+end 
+
+def breed
+  
+  @breed 
+end 
+end 
+
+
+
